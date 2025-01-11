186debf5b520dfa6ff5a8fe1e6995d72db760be1
Chen Liang <liangchen76@gmail.com>
2025-01-11 07:06:54 -0600

    prepare for azure deployment

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-# Only for streamlit deployment
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-#
-
 import os
 
 from autogen import AssistantAgent
